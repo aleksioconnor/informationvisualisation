95ab70dbab878066706dcaf4132031547e00f509
yqbk <jksyrek@gmail.com>
2018-11-12 21:40:10 +0000

    Europe map - first scratch

--- scripts.js
+++ scripts.js
@@ -163,8 +163,36 @@ function lineChartInit() {
     })
 }
 
+function europeMapInit() {
+    var w = 800;
+    var h = 600;
+
+    var projection = d3.geoMercator() //utiliser une projection standard pour aplatir les pôles, voir D3 projection plugin
+        .center([13, 52]) //comment centrer la carte, longitude, latitude
+        .translate([w / 2, h / 2]) // centrer l'image obtenue dans le svg
+        .scale([w / 1.5]);
+
+    var path = d3.geoPath()
+        .projection(projection);
+
+
+    d3.json("data/europe.json", function (json) {
+
+        //Bind data and create one path per GeoJSON feature
+        svg.append("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("stroke", "rgba(8, 81, 156, 0.2)")
+            .attr("fill", "rgba(8, 81, 156, 0.6)");
+
+    });
+}
+
 // Window onload
 window.onload = function () {
     initSlider();
     lineChartInit();
+    europeMapInit();
 };
