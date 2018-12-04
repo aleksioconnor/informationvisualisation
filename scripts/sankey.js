baf7960618e804af14bab170fbbf26889e178e59
aleksioconnor <aleksi.oconnor@gmail.com>
2018-12-04 19:14:29 +0000

    finetuning

--- scripts/sankey.js
+++ scripts/sankey.js
@@ -1,92 +1,3 @@
-// function sankeyInit() {
-
-//     d3.json("../data/sankey2013.json").then(function (data) {
-//         var width = 600
-//         var height = 370
-
-//         function format(d) {
-//             const f = d3.format(",.0f");
-//             return `${f(d)}`;
-//         }
-//         var svg = d3.select("#sankey").append("svg")
-//             .attr("width", width)
-//             .attr("height", height)
-//             .attr("style", "margin: 0 auto; display: block;")
-
-//         var sankey = d3.sankey().size([width, height]).iterations(0); // Constructs a new Sankey generator with the default settings.
-//         var {
-//             nodes,
-//             links
-//         } = sankey(data); // Computes the node and link positions for the given arguments, returning a graph representing the Sankey layout
-//         // in this case nodes is an array of nodes and links an array of links
-//         // select all inactive nodes and hide them from dom
-
-//         // draw the nodes
-//         var node = svg.append("g")
-//             // .attr("stroke", "#000")
-//             // .attr("stroke-width", 1)
-//             .selectAll("rect")
-//             .data(nodes)
-//             .enter().append("rect")
-//             .attr("x", d => d.x0)
-//             .attr("y", d => d.y0)
-//             .attr("height", d => Math.max(1, d.y1 - d.y0))
-//             .attr("width", d => d.x1 - d.x0)
-//             .attr("fill", d => {
-//                 return "gray"
-//             })
-//             .attr("opacity", 0.9)
-//             .append("title")
-//             .on("mouseover", d => {
-//                 console.log(d);
-//             })
-//             .text(d => `${d.name}\n${format(d.value)}`);
-
-
-
-//         // links
-//         var link = svg.append("g")
-//             .attr("fill", "none")
-//             .attr("stroke", "#000")
-//             .attr("stroke-opacity", 0.2)
-//             .selectAll("g")
-//             .data(links)
-//             .enter().append("g")
-//             .attr('class', 'link')
-//             .style("mix-blend-mode", "multiply")
-
-
-//         link.append("path")
-//             .attr("d", d3.sankeyLinkHorizontal())
-//             .attr("stroke-width", function (d) {
-//                 return Math.max(1, d.width)
-//             })
-//             .append("title")
-//             .text(d => `${d.source.name} → ${d.target.name}\n${format(d.value)}`);
-
-
-//         // labels
-//         var text = svg.append("g")
-//             .style("font", "14px sans-serif")
-//             .selectAll("text")
-//             .data(nodes)
-//             .enter().append("text")
-//             .attr("x", d => {
-//                 return d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 6; // if on left side, first clause
-
-//             })
-//             .attr("y", function (d) {
-//                 return (d.y1 + d.y0) / 2
-//             })
-//             .attr("dy", "0.35em")
-//             .attr("text-anchor", d => d.x0 < 10 * 4 / 5 ? "start" : "end")
-//             .text(d => d.name)
-//             .style('fill', 'black')
-//             // .style('text-shadow', '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;')
-//             .style('opacity', 1)
-//     });
-// }
-
 function sankeyInit() {
 
     function format(d) {
@@ -134,7 +45,6 @@ function sankeyInit() {
                         updateSankey = function () {
                             var date = newDate.substring(0,4); // year
                             $('#sankey-year').html(date)
-                            console.log(date);
                             var graph = data[date];
 
                             var link = links.selectAll("path")
@@ -157,7 +67,7 @@ function sankeyInit() {
 
                             linkEnter.append("title")
                                 .text(function (d) {
-                                    return d.source.name + " → " + d.target.name + "\n" + format(d.value / 1e3);
+                                    return d.source.name + " → " + d.target.name + "\n" + format(d.value);
                                 });
 
                             link.exit().remove();
@@ -171,9 +81,11 @@ function sankeyInit() {
                             var nodeEnter = node.enter().append("g");
 
                             nodeEnter.append("rect")
+                                .attr("class", "s-node")
                                 .attr("x", function (d) {
                                     return d.x0;
                                 })
+                                
                                 .attr("y", function (d) {
                                     return d.y0;
                                 })
@@ -183,7 +95,12 @@ function sankeyInit() {
                                 .attr("width", function (d) {
                                     return d.x1 - d.x0;
                                 })
-                                .attr("fill", "gray");
+                                .attr("opacity", d => {
+                                    var count = Math.max(d.sourceLinks.length, d.targetLinks.length);
+                                    // return (d.index + 0.1) * 0.02 + 0.3;
+                                    return d.value * 0.00009 + 0.3
+                                })
+                                .attr("fill", "#16262E");
 
 
                             node.select("rect").transition(t)
@@ -231,12 +148,12 @@ function sankeyInit() {
 
                             nodeEnter.append("title")
                                 .text(function (d) {
-                                    return d.name + "\n" + format(d.value / 1e3);
+                                    return d.name + "\n" + format(d.value);
                                 });
 
                             node.select("title")
                                 .text(function (d) {
-                                    return d.name + "\n" + format(d.value / 1e3);
+                                    return d.name + "\n" + format(d.value);
                                 });
 
                             node.exit().remove();
